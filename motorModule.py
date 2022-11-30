2ad2dbf2cc93eeb92edd0903872d207b15b15f8a
Izabela Kasznicka <94359676+iwinii@users.noreply.github.com>
2022-11-30 13:29:02 +0100

    Update motorModule.py

--- motorModule.py
+++ motorModule.py
@@ -13,24 +13,22 @@ CCW = 0
 
 # Setup pin layout on PI
 GPIO.setmode(GPIO.BOARD)
-
 # Establish Pins in software
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
-
 # Set the first direction you want it to spin
 GPIO.output(DIR, CW)
 class motorModules():
     def move(x):
-        if x > 0:
+        if x < 0:
             GPIO.output(DIR, 1)
-        elif x < 0:
-            GPIO.output(DIR,0)
-        pass
+        elif x > 0:
+            GPIO.output(DIR, 0)
+        else: pass
         sleep(0.1)
         GPIO.output(STEP, GPIO.HIGH)
-        sleep(.0005)
+        sleep(.00005)
         GPIO.output(STEP, GPIO.LOW)
-        sleep(.0005)
+        sleep(.00005)
 
 
